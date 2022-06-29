b3bb627e1f170590c3a6c5269a7d140367abbb79
Tahsina Bintay Azam <52793150+tahsina-azam@users.noreply.github.com>
2022-06-30 00:59:25 +0600

    first commit

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,15 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+const CoffeeStore = () => {
+    const router = useRouter();
+    console.log("router",router);
+    return (
+        <div>Coffee Store Page {router.query.id}
+        <Link href="/">
+            <a>Back to Home</a>
+        </Link>
+        </div>
+    );
+   
+}
+export default CoffeeStore;
